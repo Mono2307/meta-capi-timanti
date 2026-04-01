b1633c8b2776bd97140e78fe5f3c3e9eb4358cf7
Mono2307 <83003495+Mono2307@users.noreply.github.com>
2026-04-02 00:06:29 +0530

    Change server listen address to 0.0.0.0

--- server.js
+++ server.js
@@ -126,6 +126,6 @@ app.post('/webhook/order', async (req, res) => {
 // ====== START SERVER ======
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`);
 });
